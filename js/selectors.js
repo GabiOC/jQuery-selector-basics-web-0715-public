16a6f18224c993bb999acc56d287f3ba23e8e87e
GabiOC <oconnor.gabriele@gmail.com>
2015-09-10 17:33:45 -0400

    all done

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,6 @@
 'use-strict';
 $(document).ready(function(){
+
   h1Selector();
   liInOlSelector();
   linkSelector();
@@ -8,3 +9,22 @@ $(document).ready(function(){
 });
 
 // declare your functions here...
+function h1Selector(){
+  return $("h1");
+}
+
+function liInOlSelector(){
+  return $("ol li");
+}
+
+function linkSelector(){
+  return $("div#box4 a");
+}
+
+function imageSelector(){
+  return $("img[alt='cat sleeping']");
+}
+
+function checkboxInputSelector(){
+  return $("input[type='radio']").wrap("<span class='pinky'></span>");
+}
